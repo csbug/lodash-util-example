bc9b8754cd9b4654cea0a70933760123361cffe9
Vikram Rajput <vikramrajput1987@gmail.com>
2018-08-08 16:36:05 +0530

    lodash examples

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 // Load the full build.
 var _ = require('lodash');
-console.log(_);
+const print = console.log;
+
+const ver = function(){print('lodash version : '+ _.VERSION);}
+ver();
+
+//Lodash random number
+// var r = _.random(10);
+var r = _.random(5, 10);
+print(r);
+
+
+// Lodash determine data type
+const vals = [1, 2, 'good', [1, 2], {name: 'Peter', age: 32}];
+vals.forEach( (e) => {
+
+    if (_.isNumber(e)) {
+        print(`${e} is a number`);
+    }
+
+    if (_.isString(e)) {
+        print(JSON.stringify(e) + ' is a string');
+    }
+
+    if (_.isArray(e)) {
+        print(JSON.stringify(e) + ' is an array');
+    }
+
+    if (_.isObject(e)) {
+        print(JSON.stringify(e) + ' is an object');
+    }
+
+});
+
+//Lodash _.range function
+const rangeVals = _.range(5,10);
+print(rangeVals);
